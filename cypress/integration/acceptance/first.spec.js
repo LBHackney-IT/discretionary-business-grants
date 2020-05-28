25c61d479a64d7efd9feefa20fa56101b2ad7c85
Luca Lischetti <luca@sirlisko.com>
2020-05-28 14:01:00 +0100

    fixed index copy and cypress test

--- cypress/integration/acceptance/first.spec.js
+++ cypress/integration/acceptance/first.spec.js
@@ -7,7 +7,7 @@ context('Home page', () => {
 
   describe('Loads page', () => {
     it('has shared plan heading', () => {
-      cy.get('h1').should('have.text', 'Welcome to Shared Plan!');
+      cy.get('h1').should('have.text', 'Discretionary Business Grants!');
       cy.checkA11y('#content > h1', null, cy.terminalLog);
     });
   });
