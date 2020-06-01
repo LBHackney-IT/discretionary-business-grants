e9dc286e8b95b5d65483c31b8cb18f5dc27f9d68
Neil Kidd <neil.a.kidd@gmail.com>
2020-06-01 16:41:34 +0100

    Add contact type enum

---
+++ db/migrations/sqls/20200601153834-contact-type-down.sql
@@ -0,0 +1,1 @@
+DROP TYPE contact_type;
---
+++ db/migrations/sqls/20200601153834-contact-type-up.sql
@@ -0,0 +1,8 @@
+ CREATE TYPE contact_type AS ENUM (
+     'Agent (Authorised to act)',
+     'Owner (Sole Trader)',
+     'Partner / Employee (Acting for)',
+     'PSC of Registered Company (Person with significant control)',
+     'Trustee (Charity)'
+ );
+ 
