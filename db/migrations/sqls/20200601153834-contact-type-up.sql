ca72e8d81bc64293136e76f65b508eeb546d1a30
Neil Kidd <neil.a.kidd@gmail.com>
2020-06-02 09:40:36 +0100

    Reset contact type to use satellite table

--- db/migrations/sqls/20200601153834-contact-type-up.sql
+++
@@ -1,8 +0,0 @@
- CREATE TYPE contact_type AS ENUM (
-     'Agent (Authorised to act)',
-     'Owner (Sole Trader)',
-     'Partner / Employee (Acting for)',
-     'PSC of Registered Company (Person with significant control)',
-     'Trustee (Charity)'
- );
- 
