c5e4fb4985a65c1fe295597226dc73e2939261a6
Neil Kidd <neil.a.kidd@gmail.com>
2020-06-03 11:54:37 +0100

    Fix down migration to remove NOT NULL constraint

--- db/migrations/sqls/20200603103848-default-grant-application-date-time-down.sql
+++ db/migrations/sqls/20200603103848-default-grant-application-date-time-down.sql
@@ -1,2 +1,2 @@
 ALTER TABLE grant_application ALTER COLUMN date_time_recorded DROP DEFAULT;
-ALTER TABLE grant_application ALTER COLUMN date_time_recorded SET NULL;
+ALTER TABLE grant_application ALTER COLUMN date_time_recorded DROP NOT NULL;
