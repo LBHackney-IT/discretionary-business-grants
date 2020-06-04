4dfb4fe68a95fb1c9e96c3c2eb829e21cd3c4036
Luca Lischetti <luca@sirlisko.com>
2020-06-04 08:23:23 +0100

    improved api/url, passing all the meaningful data

--- pages/api/url/index.js
+++ pages/api/url/index.js
@@ -2,14 +2,21 @@ import getSecureUploadUrl from '../../../lib/usecases/getSecureUploadUrl';
 
 export default async (req, res) => {
   try {
-    let response = {};
-    const { documentId, fields, url } = await getSecureUploadUrl(req.body.id);
-    response.documentId = documentId;
-    response.fields = fields;
-    response.url = url;
+    const { fileKey, fileName } = req.body;
+    const { documentId, fields, url } = await getSecureUploadUrl(fileKey);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ response }));
+    res.end(
+      JSON.stringify({
+        url,
+        fields: {
+          key: `${fileKey}/${documentId}/${fileName}`,
+          ...fields,
+          'X-Amz-Server-Side-Encryption': 'AES256',
+          'X-Amz-Meta-Description': fileKey
+        }
+      })
+    );
   } catch (error) {
     res.statusCode = 500;
     res.end();
