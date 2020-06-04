6df117a7195c0f8a0994df16b3f6be9366fd06a5
Luca Lischetti <luca@sirlisko.com>
2020-06-04 08:51:00 +0100

    improved upload component and created ErrorMessage component

--- pages/upload/index.js
+++ pages/upload/index.js
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import fileUploader from 'utils/fileUpload';
 import { Button } from 'components/Form';
+import ErrorMessage from 'components/ErrorMessage/ErrorMessage';
 
 const Uploader = props => {
+  const [fileUploaded, setFileUploaded] = useState();
+  const [error, setError] = useState();
   const { register, handleSubmit } = useForm({ defaultValues: props.formData });
   const onSubmit = async formData => {
-    const fileKey = await fileUploader(formData.fileInput[0], 'test');
-    console.log(fileKey);
+    try {
+      setError(false);
+      setFileUploaded(false);
+      const fileKey = await fileUploader(formData.fileInput[0], 'test');
+      setFileUploaded(fileKey);
+    } catch {
+      setError(true);
+    }
   };
 
   return (
@@ -16,6 +26,14 @@ const Uploader = props => {
       <p>
         <input type="file" id="file" name="fileInput" ref={register} />
       </p>
+      {error && (
+        <ErrorMessage>There was a problem uploading the file.</ErrorMessage>
+      )}
+      {fileUploaded && (
+        <p>
+          File uploaded! <strong>{fileUploaded}</strong>
+        </p>
+      )}
       <Button className="govuk-button" text="Next" type="submit" />
     </form>
   );
