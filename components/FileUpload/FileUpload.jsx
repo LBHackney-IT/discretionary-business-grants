2f4233d991bb4858226cbd4cc8335d28ae5d3850
Luca Lischetti <luca@sirlisko.com>
2020-06-04 14:45:47 +0100

    added missing steps

--- components/FileUpload/FileUpload.jsx
+++ components/FileUpload/FileUpload.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react';
 
 import fileUploader from 'utils/fileUpload';
 import ErrorMessage from 'components/ErrorMessage/ErrorMessage';
+import DeleteIcon from 'components/Icons/DeleteIcon';
 
 const FileUpload = ({
   label,
   name,
   fileList = [],
   onFileUploaded,
-  documentId = 'test'
+  onFileDelete,
+  documentId = 'test',
+  isSingleFile
 }) => {
   const [error, setError] = useState();
   const [uploading, setUploading] = useState(false);
@@ -17,7 +20,7 @@ const FileUpload = ({
       setUploading(true);
       setError(false);
       const fileKey = await fileUploader(file, documentId);
-      onFileUploaded(fileKey, name);
+      onFileUploaded(fileKey, name, isSingleFile);
     } catch (e) {
       console.log(e);
       setError(true);
@@ -29,10 +32,23 @@ const FileUpload = ({
       <label className="govuk-label" htmlFor={name}>
         {label}
       </label>
+      <input
+        className="govuk-file-upload"
+        id={name}
+        name={name}
+        type="file"
+        onChange={e => e.target.files[0] && uploadFile(e.target.files[0])}
+        disabled={uploading}
+      />
       {fileList.length > 0 && (
         <ul className="govuk-list govuk-body govuk-!-margin-top-5">
           {fileList.map(file => (
-            <li key={file}>{file.split('/').pop()}</li>
+            <li key={file}>
+              {file.split('/').pop()}{' '}
+              <span role="button" onClick={() => onFileDelete(file, name)}>
+                <DeleteIcon />
+              </span>
+            </li>
           ))}
         </ul>
       )}
@@ -40,14 +56,6 @@ const FileUpload = ({
       {error && (
         <ErrorMessage>There was a problem uploading the file.</ErrorMessage>
       )}
-      <input
-        className="govuk-file-upload"
-        id={name}
-        name={name}
-        type="file"
-        onChange={e => e.target.files && uploadFile(e.target.files[0])}
-        disabled={uploading}
-      />
     </div>
   );
 };
--- components/Steps/SupplementaryInformation.jsx
+++ components/Steps/SupplementaryInformation.jsx
@@ -16,12 +16,19 @@ const SupplementaryInformation = props => {
     props.saveData({ documents: fileLists });
     Router.push(stepPath, props.nextStep);
   };
-  const handleFileUploaded = (file, name) =>
+  const handleFileUploaded = (file, name, isSingleFile) =>
     setFileLists({
       ...fileLists,
-      [name]: [...(fileLists[name] ? fileLists[name] : []), file]
+      [name]: isSingleFile
+        ? [file]
+        : [...(fileLists[name] ? fileLists[name] : []), file]
     });
-
+  const handleFileDelete = (filename, name) => {
+    setFileLists({
+      ...fileLists,
+      [name]: fileLists[name].filter(file => file !== filename)
+    });
+  };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <h1>Supplementary Information</h1>
@@ -30,12 +37,51 @@ const SupplementaryInformation = props => {
         name="file_upload"
         fileList={fileLists['file_upload']}
         onFileUploaded={handleFileUploaded}
+        onFileDelete={handleFileDelete}
       />
       <FileUpload
-        label="question 2"
+        label="Evidence of ongoing fixed property cost (a copy of the business lease, rental agreement, mortgage statement, or licence for the business premises)"
         name="file_upload2"
         fileList={fileLists['file_upload2']}
         onFileUploaded={handleFileUploaded}
+        onFileDelete={handleFileDelete}
+      />
+      <FileUpload
+        label="A copy of the business management accounts (including profit and loss account, turnover, balance sheet, and cash flow summary) for the last 12 months up to the grant application date"
+        name="file_upload3"
+        fileList={fileLists['file_upload3']}
+        onFileUploaded={handleFileUploaded}
+        onFileDelete={handleFileDelete}
+      />
+      <FileUpload
+        label="6 months up to date bank statements for the business showing the name, address and account details. Relevant fixed property costs and employee salaries paid should be highlighted"
+        name="file_upload4"
+        fileList={fileLists['file_upload4']}
+        onFileUploaded={handleFileUploaded}
+        onFileDelete={handleFileDelete}
+      />
+      <FileUpload
+        label="One form of identity for the named applicant (passport or driving licence)"
+        name="file_upload5"
+        fileList={fileLists['file_upload5']}
+        onFileUploaded={handleFileUploaded}
+        onFileDelete={handleFileDelete}
+        isSingleFile
+      />
+      <FileUpload
+        label="Company payroll information for the last 6 months showing the number of people employed and paid by the business (if available)"
+        name="file_upload6"
+        fileList={fileLists['file_upload6']}
+        onFileUploaded={handleFileUploaded}
+        onFileDelete={handleFileDelete}
+      />
+      <FileUpload
+        label="Completed State Aid declaration."
+        name="file_upload7"
+        fileList={fileLists['file_upload7']}
+        onFileUploaded={handleFileUploaded}
+        onFileDelete={handleFileDelete}
+        isSingleFile
       />
       <Button className="govuk-button" text="Next" type="submit" />
     </form>
