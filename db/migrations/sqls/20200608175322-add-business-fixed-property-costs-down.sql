9262f57984da151963c2d6221f69da2a1576f683
Neil Kidd <neil.a.kidd@gmail.com>
2020-06-08 18:59:39 +0100

    Insert fixed property costs to the DB

---
+++ db/migrations/sqls/20200608175322-add-business-fixed-property-costs-down.sql
@@ -0,0 +1,3 @@
+ALTER TABLE business DROP COLUMN fixed_property_costs_2018_2019;
+ALTER TABLE business DROP COLUMN fixed_property_costs_2019_2020;
+ALTER TABLE business DROP COLUMN fixed_property_costs_items;
---
+++ db/migrations/sqls/20200608175322-add-business-fixed-property-costs-up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE business ADD COLUMN fixed_property_costs_2018_2019 DECIMAL(11, 2);
+ALTER TABLE business ADD COLUMN fixed_property_costs_2019_2020 DECIMAL(11, 2);
+ALTER TABLE business ADD COLUMN fixed_property_costs_items VARCHAR;
