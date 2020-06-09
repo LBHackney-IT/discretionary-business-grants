dec1cfc696fa126187de2d67309ab58f2f0174f2
Neil Kidd <neil.a.kidd@gmail.com>
2020-06-09 17:46:10 +0100

    Fix business table numeric datatypes

---
+++ db/migrations/sqls/20200609164304-fix-business-numeric-data-types-down.sql
@@ -0,0 +1,6 @@
+ALTER TABLE business 
+    ALTER COLUMN rateable_value TYPE numeric,
+    ALTER COLUMN fixed_property_cost TYPE numeric,
+    ALTER COLUMN turnover_march_may_2020 TYPE numeric,
+    ALTER COLUMN turnover_2018_2019 TYPE numeric,
+    ALTER COLUMN turnover_2019_2020 TYPE numeric;
---
+++ db/migrations/sqls/20200609164304-fix-business-numeric-data-types-up.sql
@@ -0,0 +1,6 @@
+ALTER TABLE business 
+    ALTER COLUMN rateable_value TYPE numeric(11,2),
+    ALTER COLUMN fixed_property_cost TYPE numeric(11,2),
+    ALTER COLUMN turnover_march_may_2020 TYPE numeric(11,2),
+    ALTER COLUMN turnover_2018_2019 TYPE numeric(11,2),
+    ALTER COLUMN turnover_2019_2020 TYPE numeric(11,2);
