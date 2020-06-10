2cc4d9f7259a19691d7e5276571ac70806b4da20
Neil Kidd <neil.a.kidd@gmail.com>
2020-06-10 14:14:53 +0100

    Change instance of govnotify client

--- pages/api/applications/index.js
+++ pages/api/applications/index.js
@@ -1,6 +1,6 @@
 import uploadApplication from '../../../lib/usecases/uploadApplication';
 import isValidApplication from '../../../lib/usecases/validators';
-import getGovNotifyClient from '../../../lib/gateways/govNotify';
+import notifyClient from '../../../lib/gateways/govNotify';
 import { nanoid } from 'nanoid';
 
 export default async (req, res) => {
@@ -9,7 +9,8 @@ export default async (req, res) => {
     const validApplication = await isValidApplication(req.body);
     await uploadApplication({ ...validApplication, clientGeneratedId });
     if (!validApplication) throw Error('Application is invalid');
-
+    await uploadApplication({ ...req.body, clientGeneratedId });
+    await notifyClient.sendEmail(process.env.EMAIL_APPLICATION_RECEIVED_TEMPLATE_ID, request.body.contact.emailAddress);
     res.statusCode = 201;
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(clientGeneratedId));
