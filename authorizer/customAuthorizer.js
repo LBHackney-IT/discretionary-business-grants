68f0e0249831fab3bf2e12f3dd7a3e1715e2d03b
Stephen Thomson <stephen.thomson@madetech.com>
2020-06-11 13:43:14 +0100

    Allow all requests from authenticated users in allowed groups

--- authorizer/customAuthorizer.js
+++ authorizer/customAuthorizer.js
@@ -15,7 +15,13 @@ const isAllowedMethod = (allowedMethodList, authorizeEvent) => {
   return allowedMethodList.includes(authorizeEvent.requestContext.httpMethod);
 };
 
-const customAuthorize = (decodedToken, authorizeEvent) => {
+const userInAllowedGroup = (userGroups, allowedGroups) => {
+  return userGroups && userGroups.some(g => allowedGroups.includes(g));
+};
+
+const customAuthorize = allowedGroups => (decodedToken, authorizeEvent) => {
+  if (decodedToken && userInAllowedGroup(decodedToken.groups, allowedGroups))
+    return true;
   return publicList.some(publicMethod => {
     return (
       isChildPath(publicMethod.path, authorizeEvent.path) &&
