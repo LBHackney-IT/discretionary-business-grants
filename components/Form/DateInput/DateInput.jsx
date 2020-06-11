d4561e613f3a7992799b4d2e072e19bfcde680f4
Luca Lischetti <luca@sirlisko.com>
2020-06-11 16:51:04 +0100

    added DateInput component

---
+++ components/Form/DateInput/DateInput.jsx
@@ -0,0 +1,137 @@
+import { useState, useEffect, useRef } from 'react';
+import cx from 'classnames';
+import { Controller } from 'react-hook-form';
+
+const DateInput = ({ label, inputRef, error, onChange }) => {
+  const [date, setDate] = useState({ day: '', month: '', year: '' });
+  useEffect(() => {
+    const { day, month, year } = date;
+    day !== '' &&
+      month !== '' &&
+      year !== '' &&
+      onChange(`${year}-${month}-${day}`);
+  }, [date]);
+  return (
+    <div
+      className={cx('govuk-form-group', {
+        'govuk-form-group--error': error
+      })}
+    >
+      <fieldset
+        className="govuk-fieldset"
+        role="group"
+        aria-describedby={`${name}-hint`}
+      >
+        <legend className="govuk-fieldset__legend govuk-fieldset__legend--m">
+          <h1 className="govuk-fieldset__heading">{label}</h1>
+        </legend>
+        {error && (
+          <span className="govuk-error-message">
+            <span className="govuk-visually-hidden">Error:</span>{' '}
+            {error.message}
+          </span>
+        )}
+        <div className="govuk-date-input" id={name}>
+          <div className="govuk-date-input__item">
+            <div className="govuk-form-group">
+              <label
+                className="govuk-label govuk-date-input__label"
+                htmlFor={`${name}-day`}
+              >
+                Day
+              </label>
+              <input
+                className={cx(
+                  'govuk-input govuk-date-input__input govuk-input--width-2',
+                  {
+                    'govuk-input--error': error
+                  }
+                )}
+                id={`${name}-day`}
+                name={`${name}-day`}
+                type="text"
+                pattern="[0-9]*"
+                inputMode="numeric"
+                value={date.day}
+                onChange={({ target: { value } }) =>
+                  setDate({ ...date, day: value })
+                }
+                ref={inputRef}
+              />
+            </div>
+          </div>
+          <div className="govuk-date-input__item">
+            <div className="govuk-form-group">
+              <label
+                className="govuk-label govuk-date-input__label"
+                htmlFor={`${name}-month`}
+              >
+                Month
+              </label>
+              <input
+                className={cx(
+                  'govuk-input govuk-date-input__input govuk-input--width-2',
+                  {
+                    'govuk-input--error': error
+                  }
+                )}
+                id={`${name}-month`}
+                name={`${name}-month`}
+                type="text"
+                pattern="[0-9]*"
+                inputMode="numeric"
+                value={date.month}
+                onChange={({ target: { value } }) =>
+                  setDate({ ...date, month: value })
+                }
+              />
+            </div>
+          </div>
+          <div className="govuk-date-input__item">
+            <div className="govuk-form-group">
+              <label
+                className="govuk-label govuk-date-input__label"
+                htmlFor={`${name}-year`}
+              >
+                Year
+              </label>
+              <input
+                className={cx(
+                  'govuk-input govuk-date-input__input govuk-input--width-4',
+                  {
+                    'govuk-input--error': error
+                  }
+                )}
+                id={`${name}-year`}
+                name={`${name}-year`}
+                type="text"
+                pattern="[0-9]*"
+                inputMode="numeric"
+                value={date.year}
+                onChange={({ target: { value } }) =>
+                  setDate({ ...date, year: value })
+                }
+              />
+            </div>
+          </div>
+        </div>
+      </fieldset>
+    </div>
+  );
+};
+
+const ControlledDateInput = ({ control, name, rules, ...otherProps }) => {
+  const inputRef = useRef();
+  return (
+    <Controller
+      as={<DateInput inputRef={inputRef} {...otherProps} />}
+      onChange={([value]) => value}
+      name={name}
+      rules={rules}
+      onFocus={() => inputRef.current.focus()}
+      control={control}
+    ></Controller>
+  );
+};
+
+export default ControlledDateInput;
--- components/Form/index.jsx
+++ components/Form/index.jsx
@@ -1,7 +1,8 @@
 import Button from './Button/Button';
 import Checkbox from './Checkbox/Checkbox';
+import DateInput from './DateInput/DateInput';
 import TextInput from './TextInput/TextInput';
 import Radios from './Radios/Radios';
 import Select from './Select/Select';
 
-export { Button, Checkbox, Radios, Select, TextInput };
+export { Button, Checkbox, DateInput, Radios, Select, TextInput };
