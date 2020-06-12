8288ec7eaff77f64ee580ebb8e67594f483dd2b6
Luca Lischetti <luca@sirlisko.com>
2020-06-12 13:57:23 +0100

    reenabled manual entry in business address

--- components/Steps/index.jsx
+++ components/Steps/index.jsx
@@ -135,8 +135,7 @@ export const inputLabels = {
     businessAddress: {
       label: 'Business Premises Address:',
       hint:
-        "Please provide your business address in Hackney. For those in shared workspace/offices please provide the address of your shared workspace/office. For market traders please provide the most accurate  address for your market stall if you're unable to provide your exact market pitch address.",
-      supportManualEntry: false
+        "Please provide your business address in Hackney. For those in shared workspace/offices please provide the address of your shared workspace/office. For market traders please provide the most accurate  address for your market stall if you're unable to provide your exact market pitch address."
     },
     siteDescriptionId: {
       label: 'Business Premises Description:',
