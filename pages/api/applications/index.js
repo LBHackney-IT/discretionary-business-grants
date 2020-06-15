4cfa01df9f645ce0570b626540b97ee1c1f16eb1
Will Gibson <will.gibson@walterego.co.uk>
2020-06-15 17:12:57 +0100

    Select the 10 most recent applications from the database

--- pages/api/applications/index.js
+++ pages/api/applications/index.js
@@ -2,6 +2,7 @@ import uploadApplication from '../../../lib/usecases/uploadApplication';
 import isValidApplication from '../../../lib/usecases/validators';
 import sendConfirmationEmail from '../../../lib/usecases/sendConfirmationEmail';
 import { nanoid } from 'nanoid';
+import listApplications from '../../../lib/usecases/listApplications';
 
 export default async (req, res) => {
   switch (req.method) {
@@ -9,31 +10,7 @@ export default async (req, res) => {
       //TODO Return list of applications to admin
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
-      res.end(
-        JSON.stringify([
-          {
-            id: 123,
-            clientGeneratedId: 'OneTwoThree',
-            businessName: 'A Company',
-            applicationDate: '2020-06-10T17:32:28Z',
-            status: 'Unprocessed'
-          },
-          {
-            id: 124,
-            clientGeneratedId: 'OneTwoFour',
-            businessName: 'B Company',
-            applicationDate: '2020-06-9T11:32:28Z',
-            status: 'Unprocessed'
-          },
-          {
-            id: 125,
-            clientGeneratedId: 'OneTwoFive',
-            businessName: 'C Company',
-            applicationDate: '2020-06-12T14:32:28Z',
-            status: 'Unprocessed'
-          }
-        ])
-      );
+      res.end(JSON.stringify(await listApplications()));
       break;
 
     case 'POST':
