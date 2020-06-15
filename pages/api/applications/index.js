ae221a12c288b080784d72c5a86b16b7cde09a9e
Will Gibson <will.gibson@walterego.co.uk>
2020-06-15 14:37:07 +0100

    Hardcode API response for 3 applications

--- pages/api/applications/index.js
+++ pages/api/applications/index.js
@@ -9,7 +9,31 @@ export default async (req, res) => {
       //TODO Return list of applications to admin
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify("YOU'RE AN ADMIN!"));
+      res.end(
+        JSON.stringify([
+          {
+            id: 123,
+            clientGeneratedId: 'OneTwoThree',
+            businessName: 'A Company',
+            applicationDate: '2020-06-10T17:32:28Z',
+            status: 'Submitted'
+          },
+          {
+            id: 124,
+            clientGeneratedId: 'OneTwoFour',
+            businessName: 'B Company',
+            applicationDate: '2020-06-9T11:32:28Z',
+            status: 'Submitted'
+          },
+          {
+            id: 125,
+            clientGeneratedId: 'OneTwoFive',
+            businessName: 'C Company',
+            applicationDate: '2020-06-12T14:32:28Z',
+            status: 'Submitted'
+          }
+        ])
+      );
     }
     const clientGeneratedId = nanoid();
     //TODO We should 400 on invalid application and 500 on Error
