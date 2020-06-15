0b2f1677119910f4e6651abfa019d444e8100c11
Will Gibson <WillGibson@users.noreply.github.com>
2020-06-15 14:56:25 +0100

    Hardcoded API response for applications end point and a little tidy up (#118)

--- pages/api/applications/index.js
+++ pages/api/applications/index.js
@@ -4,27 +4,60 @@ import sendConfirmationEmail from '../../../lib/usecases/sendConfirmationEmail';
 import { nanoid } from 'nanoid';
 
 export default async (req, res) => {
-  try {
-    if (req.method !== 'POST') {
+  switch (req.method) {
+    case 'GET':
       //TODO Return list of applications to admin
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify("YOU'RE AN ADMIN!"));
-    }
-    const clientGeneratedId = nanoid();
-    //TODO We should 400 on invalid application and 500 on Error
-    const validApplication = await isValidApplication(req.body);
-    await uploadApplication({ ...validApplication, clientGeneratedId });
-    await sendConfirmationEmail(
-      clientGeneratedId,
-      req.body.contact.emailAddress
-    );
-    res.statusCode = 201;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(clientGeneratedId));
-  } catch (error) {
-    console.log('Application submission error:', error, 'request:', req);
-    res.statusCode = 400;
-    res.end(JSON.stringify(error.message));
+      res.end(
+        JSON.stringify([
+          {
+            id: 123,
+            clientGeneratedId: 'OneTwoThree',
+            businessName: 'A Company',
+            applicationDate: '2020-06-10T17:32:28Z',
+            status: 'Unprocessed'
+          },
+          {
+            id: 124,
+            clientGeneratedId: 'OneTwoFour',
+            businessName: 'B Company',
+            applicationDate: '2020-06-9T11:32:28Z',
+            status: 'Unprocessed'
+          },
+          {
+            id: 125,
+            clientGeneratedId: 'OneTwoFive',
+            businessName: 'C Company',
+            applicationDate: '2020-06-12T14:32:28Z',
+            status: 'Unprocessed'
+          }
+        ])
+      );
+      break;
+
+    case 'POST':
+      try {
+        const clientGeneratedId = nanoid();
+        //TODO We should 400 on invalid application and 500 on Error
+        const validApplication = await isValidApplication(req.body);
+        await uploadApplication({ ...validApplication, clientGeneratedId });
+        await sendConfirmationEmail(
+          clientGeneratedId,
+          req.body.contact.emailAddress
+        );
+        res.statusCode = 201;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(clientGeneratedId));
+      } catch (error) {
+        console.log('Application submission error:', error, 'request:', req);
+        res.statusCode = 400;
+        res.end(JSON.stringify(error.message));
+      }
+      break;
+
+    default:
+      res.statusCode = 400;
+      res.end(JSON.stringify('Invalid request method'));
   }
 };
