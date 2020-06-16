f371d78b0085267abde59cd431e6e7b95a494b8e
Neil Kidd <neil.a.kidd@gmail.com>
2020-06-16 11:06:30 +0100

    Upper case keywords

--- db/migrations/sqls/20200616094803-add-default-history-entries-up.sql
+++ db/migrations/sqls/20200616094803-add-default-history-entries-up.sql
@@ -2,5 +2,5 @@ INSERT INTO application_history
 (grant_application_id, application_state_id, date_time_recorded, user_recorded, notes)
 SELECT ga.id, 1, ga.date_time_recorded, 'system', 'Application Received'
 FROM grant_application ga
-WHERE ga.id NOT IN (select distinct grant_application_id from application_history)
+WHERE ga.id NOT IN (SELECT DISTINCT grant_application_id FROM application_history)
 ORDER BY ga.id;
