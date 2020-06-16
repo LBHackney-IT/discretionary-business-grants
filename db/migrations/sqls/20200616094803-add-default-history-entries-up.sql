959d07f31180cbe158733605d9bc44859fa0298b
Neil Kidd <neil.a.kidd@gmail.com>
2020-06-16 14:17:15 +0100

    Remove moved column

--- db/migrations/sqls/20200616094803-add-default-history-entries-up.sql
+++ db/migrations/sqls/20200616094803-add-default-history-entries-up.sql
@@ -1,6 +1,6 @@
 INSERT INTO application_history
-(grant_application_id, application_state_id, date_time_recorded, user_recorded, notes)
-SELECT ga.id, 1, ga.date_time_recorded, 'system', 'Application Received'
+(grant_application_id, date_time_recorded, user_recorded, notes)
+SELECT ga.id, ga.date_time_recorded, 'system', 'Application Received'
 FROM grant_application ga
 WHERE ga.id NOT IN (SELECT DISTINCT grant_application_id FROM application_history)
 ORDER BY ga.id;
