9f35fe56f4087aed7b334d21bfffce13638d089c
Neil Kidd <neil.a.kidd@gmail.com>
2020-06-16 13:02:50 +0100

    Remove application_state_id

--- db/migrations/sqls/20200616091329-application-history-up.sql
+++ db/migrations/sqls/20200616091329-application-history-up.sql
@@ -1,7 +1,6 @@
 CREATE TABLE application_history (
     id serial PRIMARY KEY,
     grant_application_id INTEGER REFERENCES grant_application(id) NOT NULL,
-    application_state_id  INTEGER REFERENCES application_state(id) NOT NULL,
     date_time_recorded TIMESTAMP DEFAULT NOW(),
     user_recorded VARCHAR NOT NULL,
     notes VARCHAR
