1631efd9c3011270aa9ec648a602fb0f68e81260
Will Gibson <will.gibson@walterego.co.uk>
2020-06-17 14:33:20 +0100

    Unit tests working with getDbInstance() and conatiner

---
+++ containers/AppContainer.test.js
@@ -0,0 +1,32 @@
+import AppContainer from './AppContainer';
+
+describe('AppContainer', () => {
+  let container;
+
+  beforeEach(() => {
+    process.env.API_KEY = 'notify-api-key-meow';
+    container = AppContainer.getInstance();
+  });
+
+  afterEach(() => {
+    process.env.API_KEY = undefined;
+  });
+
+  it('provides a singleton', () => {
+    const instance = AppContainer.getInstance();
+
+    expect(instance).toBeDefined();
+
+    const secondInstance = AppContainer.getInstance();
+
+    expect(instance).toEqual(secondInstance);
+  });
+
+  it('returns getDbInstance', async () => {
+    expect(await container.getDbInstance()).toBeDefined();
+  });
+
+  it('returns listApplications', () => {
+    expect(container.getListApplications()).toBeDefined();
+  });
+});
