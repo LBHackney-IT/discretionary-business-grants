cc17a2bbb117ca379fc35307ad8999e36b296502
Will Gibson <WillGibson@users.noreply.github.com>
2020-06-18 15:33:34 +0100

    Generate signed document download URLs (#144)

---
+++ pages/api/applications/[clientGeneratedId]/document/[s3Path].js
@@ -0,0 +1,25 @@
+import * as HttpStatus from 'http-status-codes';
+import { signedUrl } from '../../../../../lib/usecases/getSignedDocumentUrl';
+
+export default async (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      // eslint-disable-next-line no-case-declarations
+      const s3Path = req.query.s3Path;
+      try {
+        const result = { url: await signedUrl({ s3Path }) };
+        res.statusCode = HttpStatus.OK;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(result));
+      } catch (error) {
+        console.log('Document Signed URL error:', error, 'request:', req);
+        res.statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+        res.end(JSON.stringify('Unable to create a signed s3 document url'));
+      }
+      break;
+
+    default:
+      res.statusCode = HttpStatus.BAD_REQUEST;
+      res.end(JSON.stringify('Invalid request method'));
+  }
+};
