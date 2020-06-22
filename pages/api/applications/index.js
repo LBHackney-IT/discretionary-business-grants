2f6072688c5b9882a7fef81dee8ddd4768c4cc05
Will Gibson <will.gibson@walterego.co.uk>
2020-06-22 11:24:44 +0100

    Add test for pagination defaults

--- pages/api/applications/index.js
+++ pages/api/applications/index.js
@@ -13,8 +13,14 @@ export default async (req, res) => {
         const listApplications = container.getListApplications();
         res.statusCode = HttpStatus.OK;
         res.setHeader('Content-Type', 'application/json');
-        const currentPage = parseInt((req.query && req.query.page) || 1, 10);
-        const pageSize = parseInt((req.query && req.query.pageSize) || 10, 10);
+        const currentPage =
+          req.query && req.query.page
+            ? parseInt(req.query.page, 10)
+            : undefined;
+        const pageSize =
+          req.query && req.query.pageSize
+            ? parseInt(req.query.pageSize, 10)
+            : undefined;
         res.end(
           JSON.stringify(await listApplications({ currentPage, pageSize }))
         );
