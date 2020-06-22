755ec09b79908432402ecce5bc0780dfaed238d4
Luca Lischetti <luca@sirlisko.com>
2020-06-22 14:38:35 +0100

    added ExpandableDetails component and refactored where used

--- components/Steps/Summaries/Declaration.jsx
+++ components/Steps/Summaries/Declaration.jsx
@@ -1,99 +1,90 @@
+import ExpandableDetails from 'components/ExpandableDetails/ExpandableDetails';
+
 export default () => (
-  <details
-    className="govuk-details  govuk-!-margin-bottom-3"
-    data-module="govuk-details"
-  >
-    <summary className="govuk-details__summary">
-      <span className="govuk-details__summary-text">
-        Help with business classification
-      </span>
-    </summary>
-    <div className="govuk-details__text">
-      <div className="govuk-!-margin-bottom-3">
-        <p>
-          <strong>Covid 19 Temporary Framework Scheme</strong>
-        </p>
-        <p>
-          Following the outbreak of the Coronavirus, the European Commission has
-          approved schemes to aid businesses affected by the Coronavirus
-          outbreak on the basis of their Temporary Framework, including the
-          COVID-19 Temporary Framework scheme for the UK.
-        </p>
-        <p>
-          The maximum level of aid that a company may receive is €800 000 (€120
-          000 per undertaking active in the fishery and aquaculture sector or
-          €100 000 per undertaking active in the primary production of
-          agricultural products). This is across all UK schemes under the terms
-          of the European Commission’s Temporary Framework. The Euro equivalent
-          of the Sterling aid amount is calculated using the Commission exchange
-          rate applicable on the date the aid is offered.
-        </p>
-        <p>
-          Any aid provided under this scheme will be relevant if you wish to
-          apply, or have applied, for any other aid granted on the basis of the
-          European Commission’s Temporary Framework. You will need to declare
-          this amount to any other aid awarding body who requests information
-          from you on how much aid you have received. You must retain evidence
-          of state aid for four years after the conclusion of the UK’s
-          transition from the EU and produce it on any request from the UK
-          public authorities or the European Commission.
-        </p>
-        <p>
-          Aid may be granted to undertakings that were not in difficulty (within
-          the meaning of Article 2(18) of the General Block Exemption
-          Regulation) on 31 December 2019, but that faced difficulties or
-          entered in difficulty thereafter as a result of the COVID19 outbreak.
-        </p>
-        <p>
-          This aid is in addition to any aid that you may have received under
-          the De Minimis regulation allowing aid of up to €200,000 to any one
-          organisation over a three fiscal year period (i.e. your current fiscal
-          year and previous two fiscal years), and any other approved aid you
-          have received under other State aid rules, such as aid granted under
-          the General Block Exemption Regulation.
-        </p>
-        <p>
-          By ticking the declaration below you are confirming eligibility, in
-          principle, for aid.
-        </p>
-        <p className="govuk-hint">
-          Businesses are eligible for grants under the EU State Aid De Minimis
-          rules or the COVID-19 Temporary Framework for UK Authorities. The
-          Discretionary Grant is considered State Aid and Local Authorities have
-          a discretion to make payments to eligible recipients under either the
-          De Minimis rules or the COVID-19 Temporary Framework for UK
-          Authorities (provided all the relevant conditions are met). London
-          Borough of Hackney will be issuing payments of up to and including
-          £10,000 which can be provided under the De Minimis rules. Applicants
-          are advised to check their position before submitting applications
-          here.
-        </p>
-      </div>
-      <div className="govuk-!-margin-bottom-3">
-        <p>
-          <strong>State Aid De Minimis Rule</strong>
-        </p>
-        <p>
-          This award shall comply with the EU law on State aid on the basis
-          that, including this award, the undertaking mentioned herein shall not
-          receive more than €200,000 in total of de minimis aid within the
-          current financial year or the previous two financial years).
-        </p>
-        <p>
-          The De Minimis Regulations 1407/2013 (as published in the Official
-          Journal of the European Union L35224.12.2013) can be found at:{' '}
-        </p>
-        <p>
-          <a
-            href="https://eur-lex.europa.eu/LexUriServ/LexUriServ.do?uri=OJ:L:2013:352:0001:0008:EN:PDF"
-            target="_blank"
-            rel="noopener"
-          >
-            https://eur-lex.europa.eu/LexUriServ/LexUriServ.do?uri=OJ:L:2013:352:0001:0008:EN:PDF
-          </a>
-          .
-        </p>
-      </div>
+  <ExpandableDetails summary="Help with business classification">
+    <div className="govuk-!-margin-bottom-3">
+      <p>
+        <strong>Covid 19 Temporary Framework Scheme</strong>
+      </p>
+      <p>
+        Following the outbreak of the Coronavirus, the European Commission has
+        approved schemes to aid businesses affected by the Coronavirus outbreak
+        on the basis of their Temporary Framework, including the COVID-19
+        Temporary Framework scheme for the UK.
+      </p>
+      <p>
+        The maximum level of aid that a company may receive is €800 000 (€120
+        000 per undertaking active in the fishery and aquaculture sector or €100
+        000 per undertaking active in the primary production of agricultural
+        products). This is across all UK schemes under the terms of the European
+        Commission’s Temporary Framework. The Euro equivalent of the Sterling
+        aid amount is calculated using the Commission exchange rate applicable
+        on the date the aid is offered.
+      </p>
+      <p>
+        Any aid provided under this scheme will be relevant if you wish to
+        apply, or have applied, for any other aid granted on the basis of the
+        European Commission’s Temporary Framework. You will need to declare this
+        amount to any other aid awarding body who requests information from you
+        on how much aid you have received. You must retain evidence of state aid
+        for four years after the conclusion of the UK’s transition from the EU
+        and produce it on any request from the UK public authorities or the
+        European Commission.
+      </p>
+      <p>
+        Aid may be granted to undertakings that were not in difficulty (within
+        the meaning of Article 2(18) of the General Block Exemption Regulation)
+        on 31 December 2019, but that faced difficulties or entered in
+        difficulty thereafter as a result of the COVID19 outbreak.
+      </p>
+      <p>
+        This aid is in addition to any aid that you may have received under the
+        De Minimis regulation allowing aid of up to €200,000 to any one
+        organisation over a three fiscal year period (i.e. your current fiscal
+        year and previous two fiscal years), and any other approved aid you have
+        received under other State aid rules, such as aid granted under the
+        General Block Exemption Regulation.
+      </p>
+      <p>
+        By ticking the declaration below you are confirming eligibility, in
+        principle, for aid.
+      </p>
+      <p className="govuk-hint">
+        Businesses are eligible for grants under the EU State Aid De Minimis
+        rules or the COVID-19 Temporary Framework for UK Authorities. The
+        Discretionary Grant is considered State Aid and Local Authorities have a
+        discretion to make payments to eligible recipients under either the De
+        Minimis rules or the COVID-19 Temporary Framework for UK Authorities
+        (provided all the relevant conditions are met). London Borough of
+        Hackney will be issuing payments of up to and including £10,000 which
+        can be provided under the De Minimis rules. Applicants are advised to
+        check their position before submitting applications here.
+      </p>
     </div>
-  </details>
+    <div className="govuk-!-margin-bottom-3">
+      <p>
+        <strong>State Aid De Minimis Rule</strong>
+      </p>
+      <p>
+        This award shall comply with the EU law on State aid on the basis that,
+        including this award, the undertaking mentioned herein shall not receive
+        more than €200,000 in total of de minimis aid within the current
+        financial year or the previous two financial years).
+      </p>
+      <p>
+        The De Minimis Regulations 1407/2013 (as published in the Official
+        Journal of the European Union L35224.12.2013) can be found at:{' '}
+      </p>
+      <p>
+        <a
+          href="https://eur-lex.europa.eu/LexUriServ/LexUriServ.do?uri=OJ:L:2013:352:0001:0008:EN:PDF"
+          target="_blank"
+          rel="noopener"
+        >
+          https://eur-lex.europa.eu/LexUriServ/LexUriServ.do?uri=OJ:L:2013:352:0001:0008:EN:PDF
+        </a>
+        .
+      </p>
+    </div>
+  </ExpandableDetails>
 );
