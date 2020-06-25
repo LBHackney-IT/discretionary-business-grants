843d71f72075733cb421e70252110e80c13b1dab
Will Gibson <will.gibson@walterego.co.uk>
2020-06-25 14:51:34 +0100

    Tests passing

--- utils/api/grantOfficers.js
+++ utils/api/grantOfficers.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
 
 export const fetchGrantOfficers = async () => {
-  console.log('31561356136');
   const { data } = await axios.get(
     // Todo pass in host dynamically...
     'http://dev.discretionarybusinessgrants.hackney.gov.uk:3000/api/grant-officers'
   );
-  console.log('adrtsertag');
-  console.log(data);
   return data;
 };
