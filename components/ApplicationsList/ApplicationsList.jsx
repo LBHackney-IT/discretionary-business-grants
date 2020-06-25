88fed0562e496948c3606d592c997e3ac4158242
Will Gibson <WillGibson@users.noreply.github.com>
2020-06-25 16:11:07 +0100

    Add filter by user to list applications endpoint (#197)

--- components/ApplicationsList/ApplicationsList.jsx
+++ components/ApplicationsList/ApplicationsList.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, useMemo, useCallback } from 'react';
 import Router from 'next/router';
-import axios from 'axios';
 
 import Table from 'components/Table/Table';
 import ErrorMessage from 'components/ErrorMessage/ErrorMessage';
@@ -8,6 +7,7 @@ import { BasicSelect } from 'components/Form';
 import { fetchApplications } from 'utils/api/applications';
 
 import { APPLICATION_STATE, TYPE_OF_BUSINESS } from 'lib/dbMapping';
+import { fetchGrantOfficers } from '../../utils/api/grantOfficers';
 
 const ApplicationsList = ({
   page,
@@ -50,15 +50,16 @@ const ApplicationsList = ({
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [pageCount, setPageCount] = useState(0);
-  const [officers, setOfficiers] = useState([]);
+  const [officers, setOfficers] = useState([]);
   const checkFilter = JSON.stringify(filters);
   useEffect(() => {
     const fetchOfficers = async () => {
       try {
-        const { data } = await axios.get('/api/grant-officers');
-        setOfficiers(data.grantOfficers.map(({ identifier }) => identifier));
+        const data = await fetchGrantOfficers();
+        console.log(data);
+        setOfficers(data.grantOfficers.map(({ identifier }) => identifier));
       } catch {
-        setOfficiers(null);
+        setOfficers(null);
       }
     };
     fetchOfficers();
---
+++ utils/api/grantOfficers.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export const fetchGrantOfficers = async () => {
+  const { data } = await axios.get(
+    `${process.env.URL_PREFIX || ''}/api/grant-officers`
+  );
+  return data;
+};
