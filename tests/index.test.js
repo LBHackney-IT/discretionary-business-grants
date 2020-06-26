4338a6f06537aace642939d8f67fe7fd328d42a4
Luca Lischetti <luca@sirlisko.com>
2020-06-26 12:52:39 +0100

    moved page test in a adhoc folder

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 
-import Home from './index';
+import Home from '../pages/index';
 
 describe('Home Page', () => {
   it('should render properly', () => {
